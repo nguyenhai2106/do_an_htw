1b9fbaabc5dcaf86cdba5dba93758d9bc91639a5
Hai Nguyen <nguyentrunghai2106@gmail.com>
2022-10-21 18:10:46 +0700

    Deploy

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use("/api", braintreeRoutes);
 app.use("/api", orderRouter);
 app.use("/api", reviewRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
